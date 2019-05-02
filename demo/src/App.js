6f860047da0e2bec2b21ffa00f8db2cb18458a1b
Ron San Jose <rsanjose@Ron-San-Jose.local>
2019-05-02 13:33:35 -0700

    Fix git ignoring demo

---
+++ demo/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Example } from 'react-lib-starter';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Example />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
