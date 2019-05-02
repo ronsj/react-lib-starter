716af3da1f13a737afba33c1e6dd0800ca32ed9a
Ron San Jose <rsanjose@Ron-San-Jose.local>
2019-05-02 12:39:37 -0700

    Rollup ESM/UMD setup with Example component

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+
+import pkg from './package.json';
+
+const UMD_NAME = pkg.name
+  .split('-')
+  .map(name => name.charAt(0).toUpperCase() + name.slice(1))
+  .join('');
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      esModule: false,
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      globals: {
+        react: 'React'
+      },
+      name: UMD_NAME,
+      sourcemap: true,
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    commonjs()
+  ],
+  external: ['react', 'react-dom']
+}
---
+++ src/Example.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Example() {
+  return <p>Hello world. (Example)</p>
+}
